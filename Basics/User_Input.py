aa3ec6ce19eb7d21acaf40345c37af2657292d48
Prashantsingh0003 <prashu9838@gmail.com>
2024-09-09 08:46:16 +0530

    Intial Commit

---
+++ Basics/User_Input.py
@@ -0,0 +1,10 @@
+# Program to add two numbers with user input
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
