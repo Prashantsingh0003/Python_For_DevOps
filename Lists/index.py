a742c59eebc03602a009249240ce359c6fb37696
Prashantsingh0003 <prashu9838@gmail.com>
2024-09-24 15:31:23 +0530

    Add List file

---
+++ Lists/index.py
@@ -0,0 +1,4 @@
+my_list = [1, 2, 3, 2]
+index_of_2 = my_list.index(2)
+print("Index of 2:", index_of_2)
+
---
+++ Lists/pop.py
@@ -0,0 +1,5 @@
+my_list = [1, 2, 3]
+popped_item = my_list.pop(1)
+print(my_list)
+print("Popped item:", popped_item)
+
